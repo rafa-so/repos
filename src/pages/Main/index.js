0d3d037e617aee264fa95c2c9495af1127f3f113
Rafael Oliveira <rafa.forsoft.25@gmail.com>
2019-08-27 17:43:42 -0300

    Add style in main page
     - Install lib styled-component
     - Create styles to main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-import { Title } from './styles';
+import { Container, Form, SubmitButton } from './styles';
+
+import { FaGithub, FaPlus } from 'react-icons/fa';
 
 export default function Main(){
     return(
-        <Title>Main</Title>
+        <Container>
+            <h1>
+            <FaGithub size={ 25 } />
+                Repositórios
+            </h1>
+
+            <Form onSubmit={ () => {} }>
+                <input type="text" placeholder="" />
+                <SubmitButton>
+                    <FaPlus color="#FFF" size={ 14 } /> 
+                </SubmitButton>
+            </Form>
+        </Container>
     );
 }
