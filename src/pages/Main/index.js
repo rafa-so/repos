8802a3d5b92f40b5ce4901c5a883428f8968d74a
Rafael Oliveira <rafa.forsoft.25@gmail.com>
2019-08-28 15:06:46 -0300

    Create service API and interations
     - Create service GitHub's API
     - Create animation in button submit
     - Add endpoint get of repo name

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,21 +1,64 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 
 import { Container, Form, SubmitButton } from './styles';
 
-import { FaGithub, FaPlus } from 'react-icons/fa';
+import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
+
+import api from '../../services/api';
 
 export default function Main(){
+
+    const [newRepo, setNewRepo]           = useState('');
+    const [repositorios, setRepositorios] = useState([]);
+    const [loading, setLoading]           = useState(false);
+    
+    function handleInputChange(e) {
+        setNewRepo(e.target.value);
+    }
+
+    const handleSubmit = useCallback((e)=> {
+        e.preventDefault();
+
+        async function submit(){
+            setLoading(true);
+            try {
+                const response = await api.get(`repos/${newRepo}`);
+
+                const data = {
+                    name: response.data.full_name,
+                }
+
+                setRepositorios([...repositorios, data]);
+                setNewRepo('');
+            } catch (error) {
+                console.log(error);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        submit();
+    }, [newRepo, repositorios]);
+
     return(
         <Container>
             <h1>
-            <FaGithub size={ 25 } />
+                <FaGithub size={ 25 } />
                 Repositórios
             </h1>
 
-            <Form onSubmit={ () => {} }>
-                <input type="text" placeholder="" />
-                <SubmitButton>
-                    <FaPlus color="#FFF" size={ 14 } /> 
+            <Form onSubmit={ handleSubmit }>
+                <input type="text" 
+                    placeholder="Adicionar repositórios" 
+                    value={ newRepo } 
+                    onChange={ handleInputChange } />
+                <SubmitButton loading={ loading ? 1 : 0 }>
+                    { loading ? (
+                        <FaSpinner color="#FFF" size={ 14 } />
+                    ) : (
+                        <FaPlus color="#FFF" size={ 14 } /> 
+                    )}
+                    
                 </SubmitButton>
             </Form>
         </Container>
